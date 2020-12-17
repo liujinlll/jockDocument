b16ba3b681d9ba4c291c55d02e080bd97cee198b
liujinlei <575517574@qq.com>
2020-12-17 15:12:01 +0800

    feat: add director

---
+++ designModel/decorator.ts
@@ -0,0 +1,27 @@
+//装饰器模式
+//动态的给一个对象添加一额外的职责要比生成子类更为灵活
+
+class Component {
+    //点击事件
+    public onClick = () => {};
+}
+
+class Decorator extends Component {
+    private _component;
+
+    constructor ( component ){
+        super()
+        this._component = component;
+    }
+
+    public onClick = () => {
+        // 在这里就可以做一些其他的事情, 可以理解为一种拓展
+        this._component.onClick;
+    }
+}
+// 一个普通的Click
+const component = new Component();
+component.onClick();
+// 带有附带拓展功能的Click
+const wrapperComponent = new Decorator(component);
+wrapperComponent.onClick();
--- designModel/flyweight.ts
+++ designModel/flyweight.ts
@@ -2,10 +2,12 @@
 // 运用共享技术有效地支持大量细粒度的对象
 class FlyweightFactory {
     private flyweight;
+    
     public getFlyWeight( key ){
         if( this.flyweight[key] ){
             return this.flyweight[key]
         }
+
         const flyweight = new FlyweightFactory();
         this.flyweight[key] = flyweight;
         return flyweight;
