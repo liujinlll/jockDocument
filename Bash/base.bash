0d2d099f93b57c11225f47e0dd6226f0acf503fa
liujinlei <575517574@qq.com>
2020-12-23 10:10:13 +0800

    bash

---
+++ Bash/base.bash
@@ -0,0 +1,18 @@
+# 查看电脑安装的 shell
+echo $SHELL;
+
+# 退出 
+exit;
+
+# 查看所在路径 
+pwd;
+
+# 查看本机的Bash版本
+bash  --version;
+
+# && 只有 a成功才会执行b   || 只有 a失败才会执行b ; 无论a成功还是失败都会执行b
+
+#查看是内部命令还是外部程序命令
+type echo;
+
+
